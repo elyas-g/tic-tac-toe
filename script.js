70570560b2cc1934353ee0ef9c11b153a958ef4d
elyas-g <elyasgetnet23@gmail.com>
2025-11-20 11:19:55 +0300

    Add game logic for turns, score, announcements, events, and display updates. Add remaining HTML structure and CSS styling. Include reset button and spans for X and O for styling. Use general class name for cells to simplify event listeners.

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ function User(name, marker) {
 const player1 = {};
 const player2 = {};
 
+const turnHandler = (function () {
+  let turn = player1.marker;
+  function setTurn() {
+    if (turn == player1.marker) {
+      turn = player2.marker;
+    } else {
+      turn = player1.marker;
+    }
+  }
+  function getTurn() {
+    return turn;
+  }
+  function reset() {
+    turn = player1.marker;
+  }
+  return { setTurn, getTurn, reset };
+})();
+
 const gameBoard = (function () {
   const board = ["", "", "", "", "", "", "", "", ""];
   const winningPattern = [
@@ -17,19 +35,13 @@ const gameBoard = (function () {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  let turn = player1.marker;
   const markBoard = function (marker, position) {
     if (gameController.isGameOver()) {
-      console.log("Game is already over!");
       return;
     }
-    if (marker == turn && board[position] == "") {
+    if (marker == turnHandler.getTurn() && board[position] == "") {
       board[position] = marker;
-      if (marker == player1.marker) {
-        turn = player2.marker;
-      } else {
-        turn = player1.marker;
-      }
+      turnHandler.setTurn();
     } else {
       throw Error("can not add the maker to board successfully!");
     }
@@ -44,12 +56,8 @@ const gameBoard = (function () {
   const fullBoard = function () {
     return board.every((n) => n != "");
   };
-
-  const showBoard = function () {
-    console.log(board);
-  };
   const winnerBoard = function () {
-    for (pattern of winningPattern) {
+    for (let pattern of winningPattern) {
       let [a, b, c] = pattern;
       if (
         board[a] == player1.marker &&
@@ -73,29 +81,31 @@ const gameBoard = (function () {
     markBoard,
     resetBoard,
     fullBoard,
-    showBoard,
     winnerBoard,
   };
 })();
 
 function flowOfTheGame() {
-  let Announcement = "";
   const winnerCheck = gameBoard.winnerBoard();
   if (winnerCheck == player1.marker) {
-    gameBoard.showBoard();
-    Announcement = `${player1.name} has won the game!!`;
+    announcementControl.updateAnnouncement(
+      `${player1.name} has won the game!!`
+    );
+    displayAnnouncement();
+    scoreController.addScore(player1.marker);
     gameController.setGameOver();
   } else if (winnerCheck == player2.marker) {
-    gameBoard.showBoard();
-    Announcement = `${player2.name} has won the game!!`;
+    announcementControl.updateAnnouncement(
+      `${player2.name} has won the game!!`
+    );
+    displayAnnouncement();
+    scoreController.addScore(player2.marker);
     gameController.setGameOver();
   } else if (winnerCheck == "tie") {
-    gameBoard.showBoard();
-    Announcement = `Tie!`;
+    announcementControl.updateAnnouncement("Tie!!!");
+    displayAnnouncement();
     gameController.setGameOver();
   }
-  gameBoard.showBoard();
-  console.log(Announcement);
 }
 
 const gameController = (function () {
@@ -105,5 +115,102 @@ const gameController = (function () {
   const setGameOver = () => (gameOver = true);
   const resetGameOver = () => (gameOver = false);
 
-  return { isGameOver, setGameOver, resetGameOver };
+  return {
+    isGameOver,
+    setGameOver,
+    resetGameOver,
+  };
 })();
+
+const announcementControl = (function () {
+  let announcement = "";
+  const getAnnouncement = () => announcement;
+  const updateAnnouncement = function (text) {
+    announcement = text;
+  };
+  return {
+    getAnnouncement,
+    updateAnnouncement,
+  };
+})();
+
+const scoreController = (function () {
+  let p1score = 0;
+  let p2score = 0;
+  function addScore(value) {
+    return value == player1.marker ? p1score++ : p2score++;
+  }
+  function getScore() {
+    return { p1score, p2score };
+  }
+  return { addScore, getScore };
+})();
+
+// form
+function addGlobalEventListener(type, selector, callback) {
+  document.addEventListener(type, (e) => {
+    if (e.target.matches(selector)) {
+      callback(e);
+    }
+  });
+}
+const form = document.querySelector("form");
+const cells = document.querySelectorAll(".cell");
+const announcementBox = document.querySelector(".announcement");
+const scoreBox = document.querySelector(".score");
+const gameContainer = document.querySelector(".game-container");
+gameContainer.style.display = "none";
+addGlobalEventListener("submit", "form", (e) => {
+  e.preventDefault();
+  player1.name = form.p1name.value;
+  player2.name = form.p2name.value;
+  player1.marker = form.marker.value;
+  player2.marker = player1.marker == "X" ? "O" : "X";
+  gameContainer.style.display = "flex";
+  form.style.display = "none";
+  turnHandler.setTurn();
+  announcementControl.updateAnnouncement(
+    `Welcome ${player1.name} and ${player2.name}`
+  );
+  displayAnnouncement();
+  displayScore();
+});
+
+function displayAnnouncement() {
+  announcementBox.innerText =
+    announcementControl.getAnnouncement() != null
+      ? announcementControl.getAnnouncement()
+      : "";
+  setTimeout(() => {
+    announcementBox.innerText = "";
+  }, 2000);
+}
+
+function displayScore() {
+  values = scoreController.getScore();
+  scoreBox.innerText = `${player1.name} ${values.p1score} : ${values.p2score} ${player2.name} `;
+}
+
+addGlobalEventListener("click", ".cell", (e) => {
+  if (gameController.isGameOver()) {
+    return;
+  }
+  classList = e.target.classList;
+  currentTurn = turnHandler.getTurn();
+  gameBoard.markBoard(turnHandler.getTurn(), classList[1]);
+  e.target.innerHTML =
+    currentTurn == "X"
+      ? '<span class="x">X</span>'
+      : '<span class="o">O</span>';
+});
+
+function restart() {
+  turnHandler.reset();
+  gameBoard.resetBoard();
+  gameController.resetGameOver();
+  cells.forEach((cell) => (cell.innerHTML = ""));
+}
+
+addGlobalEventListener("click", ".restart", () => {
+  restart();
+});
